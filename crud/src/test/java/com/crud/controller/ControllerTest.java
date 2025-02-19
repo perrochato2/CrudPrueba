3019d1762e59946acc5faa73c6289760200df7ff
MARCOS_SOFTWARE <chamaco1999@gmail.com>
2025-02-18 20:32:42 -0600

    Se agrego la clase ControllerTest

---
+++ crud/src/test/java/com/crud/controller/ControllerTest.java
@@ -0,0 +1,29 @@
+package com.crud.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testEndpoint() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("CONECTADO"));
+
+    }
+
+
+}
